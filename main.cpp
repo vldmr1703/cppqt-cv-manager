911493a09c59d7fef1bb318d99b65352eb35f8af
alex.chmykhalo <alex18cas@gmail.com>
2015-10-06 09:33:20 +0300

    Iteration 2: Use structure to store data

--- main.cpp
+++ main.cpp
@@ -8,45 +8,38 @@
 //      * list of previous jobs
 //      * we would like to print our CV
 
-const int CV_LIST_SIZE = 3;
-
-char *name[CV_LIST_SIZE];
-int birthYear[CV_LIST_SIZE];
-char *skills[CV_LIST_SIZE];
-char *previousJobs[CV_LIST_SIZE];
-
-void printCV(int cvNumber)
+struct CV
 {
-    if (CV_LIST_SIZE > cvNumber)
-    {
-        std::cout << name[cvNumber] << " (" << birthYear[cvNumber] << ")" << std::endl;
-        std::cout << "=====================" << std::endl;
-        std::cout << "Skills: " << skills[cvNumber] << std::endl;
-        std::cout << "Previous work record: " << previousJobs[cvNumber] << std::endl;
-        std::cout << std::endl;
-    }
+    char *name;
+    int birthYear;
+    char *skills;
+    char *previousJobs;
+};
+
+void printCV(const CV& cv)
+{
+    std::cout << cv.name << " (" << cv.birthYear << ")" << std::endl;
+    std::cout << "=====================" << std::endl;
+    std::cout << "Skills: " << cv.skills << std::endl;
+    std::cout << "Previous work record: " << cv.previousJobs << std::endl;
+    std::cout << std::endl;
 }
 
 int main()
 {
-    name[0] = "John Jonson";
-    birthYear[0] = 1978;
-    skills[0] = "C++, Java";
-    previousJobs[0] = "IBM 1990-1995; Microsoft 1996-2002";
+    CV johnsCV;
+    johnsCV.name = "John Jonson";
+    johnsCV.birthYear = 1978;
+    johnsCV.skills = "C++, Java";
+    johnsCV.previousJobs = "IBM 1990-1995; Microsoft 1996-2002";
 
-    name[1] = "Dan Davidson";
-    birthYear[1] = 1988;
-    skills[1] = "C++, Java";
-    previousJobs[1] = "IBM 1996-1997";
+    CV dansCV {"Dan Davidson", 1988, "C++, Java", "IBM 1996-1997"};
 
-    name[2] = "Will Walker";
-    birthYear[2] = 1977;
-    skills[2] = "C++, Java";
-    previousJobs[2] = "IBM 1990-1999; Microsoft 1999-2010";
+    CV cvList[] {johnsCV, dansCV, {"Will Walker", 1977, "C++, Java", "IBM 1990-1999; Microsoft 1999-2010"} };
 
-    for (int cvNumber = 0; cvNumber < CV_LIST_SIZE; ++cvNumber)
+    for (const auto& cv: cvList)
     {
-        printCV(cvNumber);
+        printCV(cv);
     }
 
     return 0;
