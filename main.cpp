49453e9db30e979e9eec1c4b1d2c60ce424c37fe
alex.chmykhalo <alex18cas@gmail.com>
2015-10-06 09:00:06 +0300

    Initial task

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// We should keep list of programmers CVs
+// For each CV we have
+//      * name field
+//      * birthday field
+//      * list of skills
+//      * list of previous jobs
+//      * we would like to print our CV
+
+int main()
+{
+    return 0;
+}
+
