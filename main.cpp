b88ac05b344da6daeb6b15500c29298c0beb56ad
alex.chmykhalo <alex18cas@gmail.com>
2015-10-06 10:32:38 +0300

    Using arrays

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <array>
 
 // We should keep list of programmers CVs
 // For each CV we have
@@ -55,7 +56,7 @@ int main()
 
     CV dansCV("Dan Davidson", 1988, "C++, Java", "IBM 1996-1997");
 
-    CV cvList[] {johnsCV, dansCV, {"Will Walker", 197, "C++, Java", "IBM 1990-1999; Microsoft 1999-2010"} };
+    std::array<CV, 3> cvList {johnsCV, dansCV, {"Will Walker", 197, "C++, Java", "IBM 1990-1999; Microsoft 1999-2010"} };
 
     for (const auto& cv: cvList)
     {
