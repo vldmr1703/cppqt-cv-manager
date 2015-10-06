d255b86b810dc5dee5c480715a6cb902fdc35a9a
alex.chmykhalo <alex18cas@gmail.com>
2015-10-06 10:11:57 +0300

    Iteration 3: Use class to store data

--- main.cpp
+++ main.cpp
@@ -8,38 +8,46 @@
 //      * list of previous jobs
 //      * we would like to print our CV
 
-struct CV
+class CV
 {
-    char *name;
-    int birthYear;
-    char *skills;
-    char *previousJobs;
+public:
+    CV(char *name, int birthYear, char *skills, char *previousJobs) :
+        mName{name}
+      , mBirthYear{birthYear}
+      , mSkills{skills}
+      , mPreviousJobs{previousJobs}
+    {
+
+    }
+
+    void print() const
+    {
+        std::cout << mName << " (" << mBirthYear << ")" << std::endl;
+        std::cout << "=====================" << std::endl;
+        std::cout << "Skills: " << mSkills << std::endl;
+        std::cout << "Previous work record: " << mPreviousJobs << std::endl;
+        std::cout << std::endl;
+    }
+
+private:
+    char *mName;
+    int mBirthYear;
+    char *mSkills;
+    char *mPreviousJobs;
 };
 
-void printCV(const CV& cv)
-{
-    std::cout << cv.name << " (" << cv.birthYear << ")" << std::endl;
-    std::cout << "=====================" << std::endl;
-    std::cout << "Skills: " << cv.skills << std::endl;
-    std::cout << "Previous work record: " << cv.previousJobs << std::endl;
-    std::cout << std::endl;
-}
 
 int main()
 {
-    CV johnsCV;
-    johnsCV.name = "John Jonson";
-    johnsCV.birthYear = 1978;
-    johnsCV.skills = "C++, Java";
-    johnsCV.previousJobs = "IBM 1990-1995; Microsoft 1996-2002";
+    CV johnsCV {"John Jonson", 1978,"C++, Java", "IBM 1990-1995; Microsoft 1996-2002"};
 
-    CV dansCV {"Dan Davidson", 1988, "C++, Java", "IBM 1996-1997"};
+    CV dansCV("Dan Davidson", 1988, "C++, Java", "IBM 1996-1997");
 
     CV cvList[] {johnsCV, dansCV, {"Will Walker", 1977, "C++, Java", "IBM 1990-1999; Microsoft 1999-2010"} };
 
     for (const auto& cv: cvList)
     {
-        printCV(cv);
+        cv.print();
     }
 
     return 0;
